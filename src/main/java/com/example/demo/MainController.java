5320564b8f82f28a396979d5d500b560890e65c3
mdelkeroe01 <delkaroe@yahoo.com>
2018-01-25 09:28:49 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @RequestMapping("/")
+    public String showIndex(){
+        String userName="MELAKU";
+
+        return "Hello"+userName;
+
+    }
+
+
+
+}
